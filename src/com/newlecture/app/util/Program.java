c1ddfcf9cf8b7914190b2c9e99abdaf63c12f710
yamto <yamto@211.229.210.166>
2021-11-24 11:43:08 +0900

    Set/List/Map 콜렉션

--- src/com/newlecture/app/util/Program.java
+++ src/com/newlecture/app/util/Program.java
@@ -1,28 +1,42 @@
 package com.newlecture.app.util;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Program {
 
 	public static void main(String[] args) {
-		GList<Integer> list = new GList<>();
-		
+		List<Integer> list = new ArrayList<>(); // 가변
 		list.add(3);
 		list.add(5);
 		list.add(6);
 		list.add(7);
-		
-		list.add(3);
-		list.add(5);
-		list.add(6);
 		list.add(7);
-		
-		list.add(3);
-		list.add(5);
-		list.add(6);
 		list.add(7);
+		System.out.println(list.get(5));
+		System.out.println(list.size());
+		System.out.println("─────────────────");
 		
-		for(int i=0; i<list.size(); i++) {
-			System.out.printf("%d, ", list.get(i));
-		}
+		Set<Integer> set = new HashSet<>(); // 중복 제거
+		set.add(3);
+		set.add(5);
+		set.add(6);
+		set.add(7);
+		set.add(7);
+		set.add(7);
+		System.out.println(set.add(8));
+		System.out.println(set.size());
+		System.out.println("─────────────────");
+		
+		Map<String, Object> map = new HashMap<>(); // 키, 값
+		map.put("id", 3);
+		map.put("title", "Hello");
+		map.put("hit", 12);
+		System.out.println(map.get("title"));
 
 	}
 }
